0d816754185ab32fedfe2d0f3b871018c6abcbd7
vainillia <vanina.cicchellero@gmail.com>
2022-11-01 16:33:27 -0300

    segundo

--- primer_programa.py
+++ primer_programa.py
@@ -8,5 +8,7 @@ f2=np.cos(2*np.pi*t)
 fig = go.Figure()
 fig.add_trace(go.Scattergl(x=t,y=f1,mode="lines",name="Seno"))
 fig.add_trace(go.Scattergl(x=t,y=f2,mode="lines",name="Coseno"))
+fig.add_trace(go.Scattergl(x=t,y=f1+f2,mode="lines",name="Suma de funciones"))
+
 fig.show()
 
